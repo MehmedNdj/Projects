c6298be83aa426e890956227f17696300b41d8a6
MehmedNdj <mehmednedjatin13@gmail.com>
2024-04-07 18:42:48 +0300

    update and two files added

--- CandyCrush/Model/Candy.java
+++ CandyCrush/Model/Candy.java
@@ -1,2 +1,50 @@
-public class Candy {
+import java.awt.*;
+
+public abstract class Candy {
+    protected Color color;
+
+    public Candy(Color color) {
+        this.color = color;
+    }
+
+    public abstract void draw(Graphics2D g, int x, int y, int size);
 }
+
+class CircleCandy extends Candy {
+    public CircleCandy(){
+            super(Color.BLUE);
+        }
+
+    public void draw(Graphics2D g, int x, int y, int size){
+            g.setColor(color);
+            g.fillOval(x, y, size, size);
+        }
+    }
+
+
+class RectangleCandy extends Candy {
+
+    public RectangleCandy() {
+        super(Color.GREEN); // Set color to green
+    }
+
+    public void draw(Graphics2D g, int x, int y, int size) {
+        g.setColor(color);
+        g.fillRect(x, y, size, size);
+    }
+}
+
+class TriangleCandy extends Candy {
+    public TriangleCandy() {
+        super(Color.RED); // Set color to red
+    }
+
+    public void draw(Graphics2D g, int x, int y, int size) {
+        int[] xPoints = {x + size / 2, x, x + size};
+        int[] yPoints = {y, y + size, y + size};
+        g.setColor(color);
+        g.fillPolygon(xPoints, yPoints, 3);
+    }
+}
+
+
--- CandyCrush/Model/GameBoard.java
+++ CandyCrush/Model/GameBoard.java
@@ -1,2 +1,19 @@
 public class GameBoard {
+    private static final int Rows = 8;
+    private static final int Columns = 4;
+
+    private int[][] board;
+
+    public GameBoard() {
+        this.board = new int[Rows][Columns];
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < Columns; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+           // System.out.println();
+        }
+    }
 }
