f7ce1d4ac394ddc11feca7f67f7dfbdd9c22e827
MehmedNdj <mehmednedjatin13@gmail.com>
2024-04-09 17:15:06 +0300

    Update GameManager.java

--- CandyCrush/Model/GameManager.java
+++ CandyCrush/Model/GameManager.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class GameManager {
     private static final int ROWS = 8;
     private static final int COLUMNS = 4;
@@ -12,11 +14,53 @@ public class GameManager {
     }
 
     private void initializeBoard(){
+       Random random = new Random();
 
+       for (int i = 0; i< ROWS; i++){
+           for (int j = 0; j < COLUMNS; j++){
+               int candyType = random.nextInt(3);
+
+               switch (candyType){
+                   case 0:
+                       board[i][j] = new CircleCandy();
+                       break;
+                   case 1:
+                       board[i][j] = new RectangleCandy();
+                       break;
+                   case 2:
+                       board[i][j] = new TriangleCandy();
+                       break;
+               }
+           }
+       }
     }
 
     private void checkAndRemoveMatches(){
+        for (int i = 0; i < ROWS; i++){
+            for (int j = 0; j < COLUMNS - 2; j++){
+                Candy currentCandy = board[i][j];
+                if (currentCandy != null) {
+                    if(currentCandy.equals(board[i][j]) && currentCandy.equals(board[i][j + 1]) && currentCandy.equals(board[i][j + 2])) {
+                        board[i][j] = null;
+                        board[i][j + 1] = null;
+                        board[i][j + 2] = null;
+                    }
+                }
+            }
+        }
 
+        for (int j = 0; j < COLUMNS; j++) {
+            for (int i = 0; i < ROWS - 2; i++) {
+                Candy currentCandy = board[i][j];
+                if (currentCandy != null) {
+                    if (currentCandy.equals(board[i+1][j]) && currentCandy.equals(board[i + 2][j])) {
+                        board[i][j] = null;
+                        board[i + 1][j] = null;
+                        board[i + 2][j] = null;
+                    }
+                }
+            }
+        }
     }
 
     private void generateNewCandies(){
