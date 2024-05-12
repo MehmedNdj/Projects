8ac8367c0a972c38f85d4e58cb766f2cc5b26f4a
MehmedNdj <mehmednedjatin13@gmail.com>
2024-05-12 19:13:10 +0300

    Playlist added

--- MusicPlayer/src/App.jsx
+++ MusicPlayer/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './Nav'
 import MusicCard from './Cards'
 import Converter from './Converter'
 import Displayer from './Displayer'
+import PlayList from './Plalists'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <h1 className='title'>Music Player</h1>
      </div>
      <div className='converter'>
-          <h2>Your favorite music and converter , all in one place</h2> 
+          <h2>Your favorite music and converter , <br></br> all in one place</h2> 
           <Converter /> 
      </div>
      <div className='musicwrapper'>
@@ -22,6 +23,10 @@ function App() {
          <div className='displayer'>
                <Displayer />
          </div>
+      </div>
+      <div className='playlist'>
+            <h3></h3>
+            <PlayList />
       </div>
    </div>
    //Comittment issues
